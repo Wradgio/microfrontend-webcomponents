3f84aa91755533c8250dc99021dd804d5128c5fa
Marcel Zúbrik <wradgio@gmail.com>
2024-09-24 17:52:04 +0200

    App number fix

--- projects/app2/src/components/HelloWorld.ce.vue
+++ projects/app2/src/components/HelloWorld.ce.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1>App 3</h1>
+    <h1>App 2</h1>
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
